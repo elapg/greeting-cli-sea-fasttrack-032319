10ca3e839306845944b4c2f87e6354f0cf49b1e6
Mariela Gonzales <mpgonzales14@gmail.com>
2019-03-24 20:36:04 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,1 +1,8 @@
 # code the #greeting method here!
+
+ def greeting(name)
+  # puts "#{name.strip} is the name stripped"
+  # puts "#{name} is the name unstripped"
+puts  "Hello #{name.strip}. It's nice to meet you."
+ end
+
